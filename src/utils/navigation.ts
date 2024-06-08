0ac10b079e156b2f3ca2457374e1c5f1df729b52
ideas24h <raul@ideas2h.com>
2024-06-08 07:59:52 +0200

    despejo enlaces de navbar

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -1,9 +1,9 @@
 // An array of links for navigation bar
 const navBarLinks = [
   { name: "Home", url: "/" },
-  { name: "Products", url: "/products" },
+/*   { name: "Products", url: "/products" },
   { name: "Services", url: "/services" },
-  { name: "Blog", url: "/blog" },
+  { name: "Blog", url: "/blog" }, */
   { name: "Contact", url: "/contact" },
 ];
 // An array of links for footer
@@ -30,7 +30,6 @@ const footerLinks = [
 const socialLinks = {
   facebook: "https://www.facebook.com/",
   x: "https://twitter.com/",
-  github: "https://github.com/mearashadowfax/ScrewFast",
   google: "https://www.google.com/",
   slack: "https://slack.com/",
 };
